f81b4b40fdebb2efb50a3de8369f31dbe421c12c
Sugita Akira <akira3weet@gmail.com>
2021-09-24 23:33:09 -0700

    Phonemizers and transformers plugin API.

---
+++ BasicTransformers.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using WanaKanaNet;
+using OpenUtau.Api;
+
+namespace OpenUtau.Plugin.Builtin {
+    [Transformer("Romaji to Hiragana")]
+    public class RomajiToHiraganaTransformer : Transformer {
+        static Dictionary<string, string> mapping = new Dictionary<string, string>() {
+            {".", "."},
+            {",", ","},
+            {":", ":"},
+            {"/", "/"},
+            {"!", "!"},
+            {"?", "?"},
+            {"~", "~"},
+            {"-", "-"},
+            {"‘", "‘"},
+            {"’", "’"},
+            {"“", "“"},
+            {"”", "”"},
+            {"[", "["},
+            {"]", "]"},
+            {"(", "("},
+            {")", ")"},
+            {"{", "{"},
+            {"}", "}"},
+        };
+        public override string Transform(string lyric) {
+            return WanaKana.ToHiragana(lyric, new WanaKanaOptions() { CustomKanaMapping = mapping });
+        }
+    }
+
+    [Transformer("Hiragana to Romaji")]
+    public class HiraganaToRomajiTransformer : Transformer {
+        public override string Transform(string lyric) {
+            return WanaKana.ToRomaji(lyric);
+        }
+    }
+
+    [Transformer("Japanese VCV to CV")]
+    public class JapaneseVCVtoCVTransformer : Transformer {
+        public override string Transform(string lyric) {
+            if (lyric.Length > 2 && lyric[1] == ' ') {
+                return lyric.Substring(2);
+            } else {
+                return lyric;
+            }
+        }
+    }
+}
---
+++ JapaneseVCVPhonemizer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenUtau.Api;
+using OpenUtau.Core.Ustx;
+
+namespace OpenUtau.Plugin.Builtin {
+    [Phonemizer("Japanese VCV Phonemizer", "JA VCV")]
+    public class JapaneseVCVPhonemizer : Phonemizer {
+        static readonly string[] vowels = new string[] {
+            "a=ぁ,あ,か,が,さ,ざ,た,だ,な,は,ば,ぱ,ま,ゃ,や,ら,わ,ァ,ア,カ,ガ,サ,ザ,タ,ダ,ナ,ハ,バ,パ,マ,ャ,ヤ,ラ,ワ",
+            "e=ぇ,え,け,げ,せ,ぜ,て,で,ね,へ,べ,ぺ,め,れ,ゑ,ェ,エ,ケ,ゲ,セ,ゼ,テ,デ,ネ,ヘ,ベ,ペ,メ,レ,ヱ",
+            "i=ぃ,い,き,ぎ,し,じ,ち,ぢ,に,ひ,び,ぴ,み,り,ゐ,ィ,イ,キ,ギ,シ,ジ,チ,ヂ,ニ,ヒ,ビ,ピ,ミ,リ,ヰ",
+            "o=ぉ,お,こ,ご,そ,ぞ,と,ど,の,ほ,ぼ,ぽ,も,ょ,よ,ろ,を,ォ,オ,コ,ゴ,ソ,ゾ,ト,ド,ノ,ホ,ボ,ポ,モ,ョ,ヨ,ロ,ヲ",
+            "n=ん",
+            "u=ぅ,う,く,ぐ,す,ず,つ,づ,ぬ,ふ,ぶ,ぷ,む,ゅ,ゆ,る,ゥ,ウ,ク,グ,ス,ズ,ツ,ヅ,ヌ,フ,ブ,プ,ム,ュ,ユ,ル,ヴ",
+            "N=ン",
+        };
+
+        static readonly Dictionary<string, string> vowelLookup;
+
+        static JapaneseVCVPhonemizer() {
+            vowelLookup = vowels.ToList()
+                .SelectMany(line => {
+                    var parts = line.Split('=');
+                    return parts[1].Split(',').Select(cv => (cv, parts[0]));
+                })
+                .ToDictionary(t => t.Item1, t => t.Item2);
+        }
+
+        private USinger singer;
+
+        public override void SetSinger(USinger singer) => this.singer = singer;
+        public override Phoneme[] Process(Note[] notes, Note? prevNeighbour, Note? nextNeighbour) {
+            var note = notes[0];
+            if (!string.IsNullOrEmpty(note.phoneticHint)) {
+                return new Phoneme[] {
+                    new Phoneme {
+                        phoneme = note.phoneticHint,
+                    }
+                };
+            }
+            var phoneme = $"- {note.lyric}";
+            if (prevNeighbour != null) {
+                var lyric = prevNeighbour?.phoneticHint ?? prevNeighbour?.lyric;
+                var unicode = ToUnicodeElements(lyric);
+                if (vowelLookup.TryGetValue(unicode.Last(), out var vow)) {
+                    phoneme = $"{vow} {note.lyric}";
+                }
+            }
+            if (!singer.TryGetMappedOto(phoneme, note.tone, out var _)) {
+                phoneme = note.lyric;
+            }
+            return new Phoneme[] {
+                new Phoneme {
+                    phoneme = phoneme,
+                }
+            };
+        }
+    }
+}
