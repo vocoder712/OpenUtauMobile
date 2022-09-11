d8e97612814f67ba3b7e76e423a058fa55b6ca04
Sugita Akira <akira3weet@gmail.com>
2022-09-10 21:09:54 -0700

    move g2p from plugin to core

--- Data/Resources.Designer.cs
+++ Data/Resources.Designer.cs
@@ -69,45 +69,5 @@ namespace OpenUtau.Plugin.Builtin.Data {
                 return ((byte[])(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] g2p_arpabet {
-            get {
-                object obj = ResourceManager.GetObject("g2p_arpabet", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] g2p_fr {
-            get {
-                object obj = ResourceManager.GetObject("g2p_fr", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] g2p_pt {
-            get {
-                object obj = ResourceManager.GetObject("g2p_pt", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] g2p_ru {
-            get {
-                object obj = ResourceManager.GetObject("g2p_ru", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
     }
 }
